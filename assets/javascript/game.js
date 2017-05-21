2712cd9a239796288e294ae0ab18601a605e531d
juliebraun <juliebraun07113@gmail.com>
2017-05-21 16:20:36 -0600

    styled

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,6 +16,7 @@ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.l
 function reset() {
 guessesleftEl.html("");
 guessesleft=9
+guessessofarEl.html("");
 }
 function updateGuesses(key) {
   guessessofarEl.prepend(key);
@@ -41,9 +42,13 @@ function updateGuesses(key) {
         reset()
       }
     }
-
-
 }
+$("#begin").on("click", function() {
+        $(".img-responsive").animate({ opacity: "1" });
+        alert ("test");
+      });
+
+
 
 
     //  ((userGuess === "a" && computerGuess === "a") || (userGuess === "b" && computerGuess === "b") || (userGuess === "c" && computerGuess === "c") || (userGuess === "d" && computerGuess === "d") || (userGuess === "e" && computerGuess === "e") || (userGuess === "f" && computerGuess === "f") || (userGuess === "g" && computerGuess === "g") || (userGuess === "h" && computerGuess === "h") || (userGuess === "i" && computerGuess === "i") || (userGuess === "j" && computerGuess === "j") || (userGuess === "k" && computerGuess === "k") || (userGuess === "l" && computerGuess === "l") || (userGuess === "m" && computerGuess === "m") || (userGuess === "n" && computerGuess === "n") || (userGuess === "o" && computerGuess === "o") || (userGuess === "p" && computerGuess === "p") || (userGuess === "q" && computerGuess === "q") || (userGuess === "r" && computerGuess === "r") || (userGuess === "s" && computerGuess === "s") || (userGuess === "t" && computerGuess === "t") || (userGuess === "u" && computerGuess === "u") || (userGuess === "v" && computerGuess === "v") || (userGuess === "w" && computerGuess === "w") || (userGuess === "x" && computerGuess === "x") || (userGuess === "y" && computerGuess === "y") || (userGuess === "z" && computerGuess === "z") {
